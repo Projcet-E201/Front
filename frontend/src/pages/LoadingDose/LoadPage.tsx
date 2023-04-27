4d9c71796ad41a3fad78064340457f24cc7d749c
leeseunghan <seunghan4769@naver.com>
2023-04-27 10:48:20 +0900

    [front[-] sensor페이지

--- frontend/src/pages/LoadingDose/LoadPage.tsx
+++ frontend/src/pages/LoadingDose/LoadPage.tsx
@@ -1,28 +1,152 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 
 import SensorLayout from "../../layout/SensorLayout";
+import TopCard from "../../components/common/TopCard";
+import { faker } from "@faker-js/faker";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import styles from "./LoadPage.module.css";
+
+import event1 from "../../assets/event1.png";
+import event2 from "../../assets/event2.png";
+import event3 from "../../assets/event3.png";
+
+import LoadChart from "../../components/Chart/LoadChart";
 
 const LoadPage = () => {
   const navigate = useNavigate();
+  const location = useLocation();
 
-  const loads = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const [data, setData] = useState<{ x: number; [key: string]: number }[]>([]);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const currentTime = new Date().toLocaleTimeString("ko-KR", {
+        hour12: false,
+      });
+      const newEntry: any = { x: currentTime };
+      for (let i = 1; i <= 10; i++) {
+        newEntry[`Load${i}`] = faker.datatype.number({ min: 10, max: 100 });
+      }
+      setData((prevData) =>
+        prevData.length >= 10
+          ? [...prevData.slice(1), newEntry]
+          : [...prevData, newEntry]
+      );
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const datasets = [...Array(5)].map((_, i) => ({
+    id: `Load${i + 1}`,
+    data: data.map((d) => ({ x: d.x, y: d[`Load${i + 1}`] })),
+  }));
+
+  // console.log(datasets);
+  const latestData = datasets.map(
+    (dataset) => dataset.data[dataset.data.length - 1]
+  );
+
+  // console.log(latestData);
+
+  const avgData = datasets.map((dataset) => {
+    const sum = dataset.data.reduce((acc, data) => acc + data.y, 0);
+    const avg = sum / dataset.data.length;
+    return { id: dataset.id, avg };
+  });
   return (
     <SensorLayout>
-      <div>
-        {/* <button onClick={() => navigate(-1)}>메인페이지</button> */}
+      <div className={styles.topcard}>
+        <TopCard location={location.pathname} />
       </div>
-      <div>
-        <h3>부하량 페이지</h3>
-        <div>
-          {loads.map((loadId) => (
-            <div key={loadId}>
-              <button onClick={() => navigate(`/load/${loadId}`)}>
-                {loadId}
-              </button>
+      <div className={styles.midcard}>
+        <Card className={styles.card} style={{ flex: "1" }}>
+          <CardContent style={{ height: "100%" }}>
+            {/* <h3 style={{ margin: "0" }}>Event</h3> */}
+            <div
+              style={{
+                display: "flex",
+                flexWrap: "wrap",
+                alignContent: "center",
+                justifyContent: "center",
+                height: "100%",
+                width: "100%",
+                alignItems: "center",
+              }}
+            >
+              {latestData.map((data, index) => (
+                <div
+                  key={index}
+                  style={{
+                    flexDirection: "column",
+                    display: "flex",
+                    alignItems: "center",
+                    width: "20%",
+                  }}
+                >
+                  {data?.y > 90 ? (
+                    <img
+                      src={event3}
+                      alt="event3"
+                      style={{ width: 60, margin: "5px" }}
+                    />
+                  ) : data?.y > 70 ? (
+                    <img
+                      src={event2}
+                      alt="event2"
+                      style={{ width: 60, margin: "5px" }}
+                    />
+                  ) : (
+                    <img
+                      src={event1}
+                      alt="event1"
+                      style={{ width: 60, margin: "5px" }}
+                    />
+                  )}
+                  <p
+                    style={{
+                      display: "flex",
+                      justifyContent: "center",
+                      marginTop: "0",
+                    }}
+                  >
+                    Load{index + 1}
+                  </p>
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
+          </CardContent>
+        </Card>
+        <Card className={styles.card} style={{ flex: "1" }}>
+          <CardContent style={{ height: "25vh" }}>
+            <LoadChart datasets={datasets} legend={true} />
+          </CardContent>
+        </Card>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "space-between",
+        }}
+      >
+        {datasets.map((dataset, index) => (
+          // <Card className={styles.card} style={{ width: "32.3%" }}>
+          <Card
+            className={styles.card}
+            style={{ width: "49%" }}
+            onClick={() => navigate(`${index + 1}`)}
+          >
+            <CardContent style={{ height: "20vh", margin: "0" }}>
+              <h4 style={{ margin: "0" }}>Load-{index + 1}</h4>
+              <LoadChart
+                datasets={[dataset]}
+                legend={false}
+                avgData={avgData}
+              />
+            </CardContent>
+          </Card>
+        ))}
       </div>
     </SensorLayout>
   );
