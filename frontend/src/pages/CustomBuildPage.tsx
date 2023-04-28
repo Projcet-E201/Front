c83afc83fe398a2899c0b4fd489a76464b93630e
leeseunghan <seunghan4769@naver.com>
2023-04-28 09:46:56 +0900

    custom

--- frontend/src/pages/CustomBuildPage.tsx
+++ frontend/src/pages/CustomBuildPage.tsx
@@ -58,6 +58,7 @@ const CustomBuildPage = () => {
 
   const sensorChange = (event: SelectChangeEvent) => {
     setSelectedSensor(event.target.value as string);
+    setSelectedNumber("all");
   };
 
   const numberChange = (event: SelectChangeEvent) => {
@@ -103,112 +104,116 @@ const CustomBuildPage = () => {
 
   return (
     <MainLayout>
-      <div>
-        <Card className={styles.card}>
-          <CardContent>
-            <div style={{ display: "flex", justifyContent: "space-between" }}>
-              <div>
-                <h3 style={{ marginTop: "0" }}>차트 선택</h3>
-              </div>
-              <div onClick={resetHandler} style={{ cursor: "pointer" }}>
-                <RestartAltIcon />
-              </div>
+      <Card className={styles.card}>
+        <CardContent>
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <div>
+              <h3 style={{ marginTop: "0" }}>차트 선택</h3>
             </div>
-
-            <div style={{ display: "flex", justifyContent: "center" }}>
-              <FormControl fullWidth>
-                <InputLabel id="demo-simple-select-label">Machine</InputLabel>
-                <Select
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  value={selectedMachine}
-                  label="Machine"
-                  onChange={machineChange}
-                >
-                  {Array.from({ length: 12 }, (_, index) => (
-                    <MenuItem key={index + 1} value={index + 1}>
-                      {index + 1}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-
-              <FormControl fullWidth>
-                <InputLabel id="demo-simple-select-label">Sensor</InputLabel>
-                <Select
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  value={selectedSensor}
-                  label="Sensor"
-                  onChange={sensorChange}
-                  disabled={selectedMachine ? false : true}
-                >
-                  <MenuItem value={"Motor"}>Motor</MenuItem>
-                  <MenuItem value={"Vacuum"}>Vacuum</MenuItem>
-                  <MenuItem value={"AirIn"}>AirIn</MenuItem>
-                  <MenuItem value={"AirOut(kPa)"}>AirOut(kPa)</MenuItem>
-                  <MenuItem value={"AirOut(MPa)"}>AirOut(MPa)</MenuItem>
-                  <MenuItem value={"Water"}>Water</MenuItem>
-                  <MenuItem value={"마모량"}>마모량</MenuItem>
-                  <MenuItem value={"부하량"}>부하량</MenuItem>
-                  <MenuItem value={"회전속도"}>회전속도</MenuItem>
-                </Select>
-              </FormControl>
-              <FormControl fullWidth>
-                <InputLabel id="demo-simple-select-label">
-                  Sensor Number
-                </InputLabel>
-                <Select
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  value={selectedNumber}
-                  label="Sensor Number"
-                  onChange={numberChange}
-                  disabled={selectedSensor ? false : true}
-                >
-                  <MenuItem value={"all"}>all</MenuItem>
-                  {numberOptions}
-                </Select>
-              </FormControl>
+            <div onClick={resetHandler} style={{ cursor: "pointer" }}>
+              <RestartAltIcon />
             </div>
+          </div>
+
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Machine</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={selectedMachine}
+                label="Machine"
+                onChange={machineChange}
+              >
+                {Array.from({ length: 12 }, (_, index) => (
+                  <MenuItem key={index + 1} value={index + 1}>
+                    {index + 1}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Sensor</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={selectedSensor}
+                label="Sensor"
+                onChange={sensorChange}
+                disabled={selectedMachine ? false : true}
+              >
+                <MenuItem value={"Motor"}>Motor</MenuItem>
+                <MenuItem value={"Vacuum"}>Vacuum</MenuItem>
+                <MenuItem value={"AirIn"}>AirIn</MenuItem>
+                <MenuItem value={"AirOut(kPa)"}>AirOut(kPa)</MenuItem>
+                <MenuItem value={"AirOut(MPa)"}>AirOut(MPa)</MenuItem>
+                <MenuItem value={"Water"}>Water</MenuItem>
+                <MenuItem value={"마모량"}>마모량</MenuItem>
+                <MenuItem value={"부하량"}>부하량</MenuItem>
+                <MenuItem value={"회전속도"}>회전속도</MenuItem>
+              </Select>
+            </FormControl>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">
+                Sensor Number
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={selectedNumber}
+                label="Sensor Number"
+                onChange={numberChange}
+                disabled={selectedSensor ? false : true}
+              >
+                <MenuItem value={"all"}>all</MenuItem>
+                {numberOptions}
+              </Select>
+            </FormControl>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "end",
+              marginTop: "10px",
+            }}
+          >
             <div
               style={{
                 display: "flex",
-                justifyContent: "end",
-                marginTop: "10px",
+                alignItems: "end",
+                marginRight: "20px",
+                width: "310px",
               }}
             >
-              <div
-                style={{
-                  display: "flex",
-                  alignItems: "end",
-                  marginRight: "20px",
-                  width: "310px",
-                }}
-              >
-                <h5 style={{ margin: "0" }}>선택된 차트: &nbsp;&nbsp; </h5>
-                <h4 style={{ margin: "0" }}>
-                  {selectedMachine && <span>Machine{selectedMachine} -</span>}{" "}
-                  {selectedSensor && <span>{selectedSensor} -</span>}{" "}
-                  {selectedNumber && <span>{selectedNumber}</span>}{" "}
-                </h4>
-              </div>
-              <button style={{ height: "40px" }} onClick={addChartHandler}>
-                ADD CHART
-              </button>
+              <h5 style={{ margin: "0" }}>선택된 차트: &nbsp;&nbsp; </h5>
+              <h4 style={{ margin: "0" }}>
+                {selectedMachine && <span>Machine{selectedMachine} -</span>}{" "}
+                {selectedSensor && <span>{selectedSensor} -</span>}{" "}
+                {selectedNumber && <span>{selectedNumber}</span>}{" "}
+              </h4>
             </div>
-          </CardContent>
-        </Card>
-      </div>
+            <button style={{ height: "40px" }} onClick={addChartHandler}>
+              ADD CHART
+            </button>
+          </div>
+        </CardContent>
+      </Card>
+
       <div
         style={{
           display: "flex",
           flexWrap: "wrap",
-          justifyContent: "space-between",
+          justifyContent: chartList.length > 1 ? "space-between" : "center",
         }}
       >
         {chartList.map((chart, index) => (
-          <Card className={styles.card} style={{ width: "42vw" }}>
+          <Card
+            className={styles.card}
+            style={{
+              width: chartList.length > 1 ? "49%" : "100%",
+            }}
+          >
             <CardContent>
               <div style={{ display: "flex", justifyContent: "space-between" }}>
                 <h4 style={{ marginTop: "0" }}>
@@ -216,7 +221,7 @@ const CustomBuildPage = () => {
                 </h4>
                 <button onClick={() => deleteChart(index)}>삭제</button>
               </div>
-              <div style={{ height: "50vh" }}>
+              <div style={{ height: chartList.length > 2 ? "23vh" : "55vh" }}>
                 {chart.sensor === "Motor" && <CardMotorChart />}
               </div>
             </CardContent>
