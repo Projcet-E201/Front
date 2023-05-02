ab8b3c67a0a2a245d609635e86771c9c01c85ee5
leeseunghan <seunghan4769@naver.com>
2023-05-02 09:10:46 +0900

    -[fornt] layout

--- frontend/src/layout/SensorLayout.tsx
+++ frontend/src/layout/SensorLayout.tsx
@@ -12,21 +12,32 @@ const SensorLayout = ({ children }: Props) => {
   const [childrenHeight, setChildrenHeight] = useState<number>(0);
   const childrenRef = useRef<HTMLDivElement>(null);
 
+  const [leftNavWidth, setLeftNavWidth] = useState<number>(0);
+  const leftNavRef = useRef<HTMLDivElement>(null);
+
   useEffect(() => {
     if (childrenRef.current) {
       setChildrenHeight(childrenRef.current.clientHeight);
     }
     // console.log("칠드런 높이", childrenHeight);
   }, [children]);
+
+  // useEffect(() => {
+  //   if (leftNavRef.current) {
+  //     setLeftNavWidth(leftNavRef.current.clientWidth);
+  //   }
+  //   console.log(leftNavWidth, "left");
+  // }, [leftNavWidth]);
+
   return (
     <div>
-      <div ref={childrenRef}>
-        <NavBar />
+      <div ref={childrenRef} style={{ display: "fixed" }}>
+        <NavBar leftNavWidth={leftNavWidth} />
       </div>
-      <div style={{ display: "flex" }}>
-        <div style={{ width: "13%" }}>
-          <LeftNav childrenHeight={childrenHeight} />
-        </div>
+      <div style={{ display: "flex", width: "100%" }}>
+        {/* <div ref={leftNavRef}> */}
+        <LeftNav childrenHeight={childrenHeight} />
+        {/* </div> */}
         <div style={{ width: "100%" }}>
           <div className={styles.children}>{children}</div>
         </div>
