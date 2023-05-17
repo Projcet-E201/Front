e280196b220b392945273c92ab7ab997ffdc9ab0
leeseunghan <seunghan4769@naver.com>
2023-05-17 16:15:38 +0900

    main

--- frontend/src/pages/AirOut2/AirOut2Page.tsx
+++ frontend/src/pages/AirOut2/AirOut2Page.tsx
@@ -8,6 +8,9 @@ import CardContent from "@mui/material/CardContent";
 import TopCard from "../../components/common/TopCard";
 import styles from "./AirOut2Page.module.css";
 
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
+
 import event1 from "../../assets/event1.png";
 import event2 from "../../assets/event2.png";
 import event3 from "../../assets/event3.png";
@@ -94,94 +97,115 @@ const AirOut2Page = () => {
       <div className={styles.topcard}>
         <TopCard location={location.pathname} />
       </div>
-      <div className={styles.midcard}>
-        <Card className={styles.card} style={{ flex: "1" }}>
-          <CardContent style={{ height: "100%" }}>
-            {/* <h3 style={{ margin: "0" }}>Event</h3> */}
-            <div
-              style={{
-                display: "flex",
-                flexWrap: "wrap",
-                alignContent: "center",
-                justifyContent: "center",
+      {airOutMpaData.length === 0 ? (
+        <Card className={styles.card}>
+          <CardContent style={{ height: "70vh" }}>
+            <Box
+              sx={{
                 height: "100%",
-                width: "100%",
+                display: "flex",
+                flexDirection: "column",
                 alignItems: "center",
+                justifyContent: "center",
               }}
             >
-              {latestData.map((data: any, index: number) => (
+              <CircularProgress />
+              <h3>Motor 데이터를 불러오는 중 입니다...</h3>
+            </Box>
+          </CardContent>
+        </Card>
+      ) : (
+        <div>
+          <div className={styles.midcard}>
+            <Card className={styles.card} style={{ flex: "1" }}>
+              <CardContent style={{ height: "100%" }}>
+                {/* <h3 style={{ margin: "0" }}>Event</h3> */}
                 <div
-                  key={index}
                   style={{
-                    flexDirection: "column",
                     display: "flex",
+                    flexWrap: "wrap",
+                    alignContent: "center",
+                    justifyContent: "center",
+                    height: "100%",
+                    width: "100%",
                     alignItems: "center",
-                    width: "20%",
-                    cursor: "pointer",
-                    margin: "0px",
                   }}
-                  onClick={() => navigate(`${index + 1}`)}
                 >
-                  {data?.y > 800 ? (
-                    <img
-                      src={event3}
-                      alt="event3"
-                      style={{ width: 60, margin: "5px" }}
-                    />
-                  ) : data?.y > 700 ? (
-                    <img
-                      src={event2}
-                      alt="event2"
-                      style={{ width: 60, margin: "5px" }}
-                    />
-                  ) : (
-                    <img
-                      src={event1}
-                      alt="event1"
-                      style={{ width: 60, margin: "5px" }}
-                    />
-                  )}
-                  <p
-                    style={{
-                      display: "flex",
-                      justifyContent: "center",
-                      marginTop: "0",
-                    }}
-                  >
-                    AO(Mpa){index + 1}
-                  </p>
+                  {latestData.map((data: any, index: number) => (
+                    <div
+                      key={index}
+                      style={{
+                        flexDirection: "column",
+                        display: "flex",
+                        alignItems: "center",
+                        width: "20%",
+                        cursor: "pointer",
+                        margin: "0px",
+                      }}
+                      onClick={() => navigate(`${index + 1}`)}
+                    >
+                      {data?.y > 800 ? (
+                        <img
+                          src={event3}
+                          alt="event3"
+                          style={{ width: 60, margin: "5px" }}
+                        />
+                      ) : data?.y > 700 ? (
+                        <img
+                          src={event2}
+                          alt="event2"
+                          style={{ width: 60, margin: "5px" }}
+                        />
+                      ) : (
+                        <img
+                          src={event1}
+                          alt="event1"
+                          style={{ width: 60, margin: "5px" }}
+                        />
+                      )}
+                      <p
+                        style={{
+                          display: "flex",
+                          justifyContent: "center",
+                          marginTop: "0",
+                        }}
+                      >
+                        AO(Mpa){index + 1}
+                      </p>
+                    </div>
+                  ))}
                 </div>
-              ))}
-            </div>
-          </CardContent>
-        </Card>
-        <Card className={styles.card} style={{ flex: "2" }}>
-          <CardContent style={{ height: "25vh" }}>
-            <AirOut2Chart datasets={airOutMpaData} legend={true} />
-          </CardContent>
-        </Card>
-      </div>
-      <div
-        style={{
-          display: "flex",
-          flexWrap: "wrap",
-          justifyContent: "space-between",
-        }}
-      >
-        {airOutMpaData.map((dataset: any, index: number) => (
-          // <Card className={styles.card} style={{ width: "32.3%" }}>
-          <Card
-            className={styles.card}
-            style={{ width: "49%", cursor: "pointer" }}
-            onClick={() => navigate(`${index + 1}`)}
+              </CardContent>
+            </Card>
+            <Card className={styles.card} style={{ flex: "2" }}>
+              <CardContent style={{ height: "25vh" }}>
+                <AirOut2Chart datasets={airOutMpaData} legend={true} />
+              </CardContent>
+            </Card>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexWrap: "wrap",
+              justifyContent: "space-between",
+            }}
           >
-            <CardContent style={{ height: "25vh", margin: "0" }}>
-              <h4 style={{ margin: "0" }}>AirOut-MPa{index + 1}</h4>
-              <AirOut2Chart datasets={[dataset]} legend={false} />
-            </CardContent>
-          </Card>
-        ))}
-      </div>
+            {airOutMpaData.map((dataset: any, index: number) => (
+              // <Card className={styles.card} style={{ width: "32.3%" }}>
+              <Card
+                className={styles.card}
+                style={{ width: "49%", cursor: "pointer" }}
+                onClick={() => navigate(`${index + 1}`)}
+              >
+                <CardContent style={{ height: "25vh", margin: "0" }}>
+                  <h4 style={{ margin: "0" }}>AirOut-MPa{index + 1}</h4>
+                  <AirOut2Chart datasets={[dataset]} legend={false} />
+                </CardContent>
+              </Card>
+            ))}
+          </div>
+        </div>
+      )}
     </SensorLayout>
   );
 };
